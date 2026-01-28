daaed6e0184553deeb6293b8438e226a870cbcf8
Drishti Sharma <drishtisharma14052007@gmail.com>
2026-01-28 23:31:21 +0530

    final changes

--- app.py
+++ app.py
@@ -100,5 +100,6 @@ def accept_alert(alert_id):
     return redirect('/helper')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
